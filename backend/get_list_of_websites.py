ae53f408e94f6261158823b47ed55a33b899e418
Jayce <97198910+jasjs1@users.noreply.github.com>
2024-12-14 15:38:22 -0600

    corrected file path

--- backend/get_list_of_websites.py
+++ backend/get_list_of_websites.py
@@ -1,10 +1,10 @@
 import json
 
-def extract_titles(file_path="password.json"):
+def extract_titles(file_path="passwords.json"):
     try:
         with open(file_path, 'r') as file:
             data = json.load(file) 
-            titles = list(data.keys())
+            titles = list(data.keys())  
             return titles
     except FileNotFoundError:
         print("The passwords file was not found.")
